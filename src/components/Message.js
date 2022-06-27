428079582e2fb356c9944f3e9b0770a155c566e7
Jorge <JorgeIVelez93@gmail.com>
2022-06-27 18:04:30 -0400

    recreated Twitter feed

---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+function Message(props) {
+return    (<p className="message">
+{props.text}
+</p>)
+}
+export default Message
---
+++ src/components/Timestamp.js
@@ -0,0 +1,4 @@
+function Timestamp(props) {
+    return <span className="timestamp">{props.stamp}</span>
+}
+export default Timestamp
